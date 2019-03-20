a23de9fcb63bcd7cce70e514a99b717c69ece719
songtuyen <songtuyen97@gmail.com>
2019-03-20 22:01:21 +0700

    updated DB mongo - monan model

--- apps/common/mongoose.js
+++ apps/common/mongoose.js
@@ -88,91 +88,33 @@ var schema_dichvu = mongoose.Schema({
                 }]
             }]
         },
-        monan : [{
-            ten : String, 
+        danhmuc : [{
+            ten : String,
             mota : String,
-            hinhanh_url : String,
-            gia : Number,
-            soluong : Number,
-            trungbinhsao : Number,
-            danhgia : [{
-                    comment : String,
-                    star : Number,
-                    //?? id_user
-                    nguoimua : {
-                        id : String,
-                        name : String,
-                        avatar_url : String
-                    }
-            }],
-            danhmuc : {
-                ten : String,
-                mota : String
-            }
+            monan : [{
+                ten : String, 
+                mota : String,
+                hinhanh_url : String,
+                gia : Number,
+                soluong : Number,
+                trungbinhsao : Number,
+                danhgia : [{
+                        comment : String,
+                        star : Number,
+                        //?? id_user
+                        nguoimua : {
+                            id : String,
+                            name : String,
+                            avatar_url : String
+                        }
+                }]
+            }]
         }]
+        
     }
 });
-// var schema_khachhang = mongoose.Schema({
-//     username : String,
-//     password : String,
-//     role : [{
-//         name_role : String,
-//         description : String,
-//         licensed : Boolean,
-//         permission : [{
-//             name_per : String,
-//             description : String,
-//             per_detail : {
-//                 view : Boolean,
-//                 create : Boolean,
-//                 update : Boolean,
-//                 delete : Boolean
-//             }
-//         }]
-//     }],
-//     infomation : {
-//         name : String,
-//         address : String, 
-//         phonenumber : String,
-//         avatar_url : String,
-//         danhgia : [{
-//             comment : String,
-//             star : Number,
-//             //? id_monan
-//             monan : {
-//                 id : String,
-//                 ten : String, 
-//             }
-//         }],
-//         order: [{
-//             tongtien: Number,
-//             giodat : Date,
-//             dichvu : {
-//                 ten : String,
-//                 avatar_url : String,
-//                 id : String
-//             },
-//             //id_diachi
-//             diachi : {
-//                 tenthanhpho: String,
-//                 tenquan : String
-//             },
-//             order_detail : [{
-//                 //id_monan : String,
-//                 monan : {
-//                     ten : String,
-//                     id : String
-//                 },
-//                 soluong : Number,
-//                 gia: Number
-//             }]
-//         }],
-//     }
-// });
 const model_dichvu = mongoose.model("user", schema_dichvu);
-//const model_khachhang = mongoose.model("user", schema_khachhang);
 
 module.exports = {
     model_dichvu : model_dichvu,
-//    model_khachhang : model_khachhang
 };
