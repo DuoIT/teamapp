8262b6fa1587a31f4cf60a04f80dd1d6bc031612
songtuyen <songtuyen97@gmail.com>
2019-04-18 14:33:53 +0700

    updated update avatar_url

--- apps/common/mongoose.js
+++ apps/common/mongoose.js
@@ -121,7 +121,73 @@ var schema_dichvu = mongoose.Schema({
 
     }
 });
+var schema_order = mongoose.Schema({
+        tongtien: Number,
+        giodat: Date,
+        trangthai: String,
+        diachi: {
+            tenthanhpho: String,
+            tenquan: String
+        },
+        information: {
+            ten: String,
+            id: String
+        },
+        dichvu: {
+            ten: String,
+            id: String
+        },
+        order_detail: [{
+            monan: {
+                ten: String,
+                id: String
+            },
+            soluong: Number,
+            gia: Number
+        }]
+})
+var schema_checkout = mongoose.Schema({
+        id_monan: String,
+        id_dichvu: String
+})
 const model_dichvu = mongoose.model("user", schema_dichvu);
+const model_order = mongoose.model("orders", schema_order);
+const model_checkout = mongoose.model("checkout", schema_checkout);
+// model_order.create({
+//     tongtien: 30000,
+//     giodat: new Date(),
+//     trangthai: "chưa thanh toán",
+//     diachi: {
+//         tenthanhpho: "Da nang",
+//         tenquan: "lien chieu"
+//     },
+//     information: {
+//         ten: "chinh",
+//         id: "5cb6187df335fd00047121c4"
+//     },
+//     dichvu: {
+//         ten: "songtuyen",
+//         id: "5c9350f032aacf04b862e50e"
+//     },
+//     order_detail: [{
+//         monan: {
+//             ten: "conchien",
+//             id: "5cb73d5ef7d6630004dbf4b7"
+//         },
+//         soluong: 3,
+//         gia: 10000
+//     }]
+// }, function(err, result) {
+//     if(err) console.log("loioday");
+// })
+// model_checkout.create({
+//     id_monan: "5cb73d5ef7d6630004dbf4b7",
+//     id_dichvu: "5c9350f032aacf04b862e50e"
+// }, function(err, result) {
+//     if(err) console.log("loioday");
+// })
 module.exports = {
     model_dichvu: model_dichvu,
+    model_order: model_order,
+    model_checkout: model_checkout
 };
